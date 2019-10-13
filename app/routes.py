f2cd6922e312cbba5e3b446f650f6486de72088d
Julian Lawrance <julian.lawrance@gmail.com>
2019-10-13 22:08:35 +0100

    Add basic HTML templates for a few endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,36 @@
 from app import app
+from flask import render_template
 
 # home page
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    return render_template('base.html')
+
+# single-album recommendation page
+@app.route('/album')
+def album():
+    album = {
+        'name': 'Metaprogramação',
+        'artist': 'Deaf Kids'
+    }
+    return render_template('album.html', title='Album', album=album)
+
+# multiple-album recommendation page
+@app.route('/albums')
+def albums():
+    albums = [
+        {
+            'name': 'Metaprogramação',
+            'artist': 'Deaf Kids'
+        },
+        {
+            'name': 'The Dance of the Moon and the Sun',
+            'artist': 'Natural Snow Buildings'
+        },
+        {
+            'name': 'Charli',
+            'artist': 'Charli XCX'
+        }
+    ]
+    return render_template('albums.html', albums=albums)
