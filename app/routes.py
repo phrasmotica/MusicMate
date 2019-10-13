bc5daa8c13d13160a158b4438b42707dcb482063
Julian Lawrance <julian.lawrance@gmail.com>
2019-10-13 22:10:11 +0100

    Add login page using Flask-WTF

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,25 @@
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
 
 # home page
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('base.html')
+
+# login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            'Login requested for user {}, remember_me={}'.format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 # single-album recommendation page
 @app.route('/album')
