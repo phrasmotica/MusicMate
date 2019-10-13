e8a550cc51c92a4aa6d787847a04edf1dc50bac3
Julian Lawrance <julian.lawrance@gmail.com>
2019-10-13 22:12:02 +0100

    Create shell context function for automatic imports

--- musicmate.py
+++ musicmate.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Review
+
+# allow us to run 'flask shell' and have all the imports we need from the get-go
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Review': Review}
